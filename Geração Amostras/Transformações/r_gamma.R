5b15eef8b284ced80097126a3649547a0c658c54
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2022-10-31 20:09:46 -0300

    Update r_gamma.R

--- Geração Amostras/Transformações/r_gamma.R
+++ Geração Amostras/Transformações/r_gamma.R
@@ -1,11 +1,16 @@
-r_exp <- function(n, lambda){
-  u=runif(n)
-  x=-(1/lambda)*log(1-u)
-  return(x)
-}
-
 r_gamma <- function(n, r, lambda){
   library(tidyverse)
+  
+  # Função para gerar exponenciais
+  
+  r_exp <- function(n, lambda){
+    u=runif(n)
+    x=-(1/lambda)*log(1-u)
+    return(x)
+  }
+  
+  # Soma de r exponenciais para gerar gama
+  
   x <- 1:n %>% 
     map(~r_exp(r,lambda)) %>% 
     map_dbl(sum)

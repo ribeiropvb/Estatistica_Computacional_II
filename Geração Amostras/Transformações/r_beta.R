fcfbaa19a5feacbff1ee576e0bafb1fe2161c4b0
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2022-10-31 20:07:02 -0300

    Update r_beta.R

--- Geração Amostras/Transformações/r_beta.R
+++ Geração Amostras/Transformações/r_beta.R
@@ -1,18 +1,24 @@
-r_exp <- function(n, lambda){
-  u=runif(n)
-  x=-(1/lambda)*log(1-u) # aplicando a funcao inversa 
-  return(x)
-}
-
-r_gamma <- function(n, r, lambda){
-  library(tidyverse)
-  x <- 1:n %>% 
-    map(~r_exp(r,lambda)) %>% 
-    map_dbl(sum)
-  return(x)
-}
-
 r_beta <- function(n,r,b){
+  library(tidyverse)
+  
+  # Função para gerar exponenciais
+  
+  r_exp <- function(n, lambda){
+    u=runif(n)
+    x=-(1/lambda)*log(1-u) # aplicando a funcao inversa 
+    return(x)
+  }
+  
+  # Função para gerar Gama
+  
+  r_gamma <- function(n, r, lambda){
+    library(tidyverse)
+    x <- 1:n %>% 
+      map(~r_exp(r,lambda)) %>% 
+      map_dbl(sum)
+    return(x)
+  }
+  
   X <- r_gamma(n,r,1)
   Y <- r_gamma(n,b,1)
   x <- X/(Y+X)

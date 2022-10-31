322dc21569cc6cdd9d0b09e820de93d8962687ec
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2022-10-31 19:56:20 -0300

    Add files via upload
    Gama - Soma de Exponenciais (r inteiro)

---
+++ Geração Amostras/Transformações/r_gamma.R
@@ -0,0 +1,12 @@
+r_exp <- function(n, lambda){
+  u=runif(n)
+  x=-(1/lambda)*log(1-u)
+  return(x)
+}
+
+r_gamma <- function(n, r, lambda){
+  x <- 1:n %>% 
+    map(~r_exp(r,lambda)) %>% 
+    map_dbl(sum)
+  return(x)
+}

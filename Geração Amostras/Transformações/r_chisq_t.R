408eb7ff93dff21fc013e40592c6055607e9411d
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2022-11-06 15:58:55 -0300

    Update r_chisq_t.R

--- Geração Amostras/Transformações/r_chisq_t.R
+++ Geração Amostras/Transformações/r_chisq_t.R
@@ -1,15 +1,28 @@
 rchisq_t <- function(n, gl){
   rnorm_mi <- function(n){
-    u <- runif(n/2)
-    v <- runif(n/2)
-    
-    z1 <- cos(2*pi*v)*sqrt(-2*log(u))
-    z2 <- cos(2*pi*u)*sqrt(-2*log(v))
-    x <- c(z1,z2)
+    if(n %% 2 == 1){
+      n <- n+1
+      u <- runif(n/2)
+      v <- runif(n/2)
+      
+      z1 <- cos(2*pi*v)*sqrt(-2*log(u))
+      z2 <- cos(2*pi*u)*sqrt(-2*log(v))
+      x <- c(z1,z2)
+      x <- x[-1]
+    }else{
+      u <- runif(n/2)
+      v <- runif(n/2)
+      
+      z1 <- cos(2*pi*v)*sqrt(-2*log(u))
+      z2 <- cos(2*pi*u)*sqrt(-2*log(v))
+      x <- c(z1,z2)
+    }
     return(x)
   }
   x <- 1:n %>%
-    map(function(x,n) rnorm_mi(n = gl)^2) %>% 
+    map(function(x,n) rnorm_mi(n = gl)) %>% 
+    map(function(x) x^2) %>% 
     map_dbl(sum)
+  if(x %% 2 == 1) x <- x[-1]
   return(x)
 }

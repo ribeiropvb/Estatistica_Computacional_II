53289206fc12f5cb9b6a21e7321c9863ebbb11a0
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2022-11-06 15:59:41 -0300

    Update r_norm_mi.R

--- Geração Amostras/Método Inversa/r_norm_mi.R
+++ Geração Amostras/Método Inversa/r_norm_mi.R
@@ -1,9 +1,20 @@
-r_norm_mi <- function(n){
-  u <- runif(n/2)
-  v <- runif(n/2)
-  
-  z1 <- cos(2*pi*v)*sqrt(-2*log(u))
-  z2 <- cos(2*pi*u)*sqrt(-2*log(v))
-  x <- c(z1,z2)
+rnorm_mi <- function(n){
+  if(n %% 2 == 1){
+    n <- n+1
+    u <- runif(n/2)
+    v <- runif(n/2)
+    
+    z1 <- cos(2*pi*v)*sqrt(-2*log(u))
+    z2 <- cos(2*pi*u)*sqrt(-2*log(v))
+    x <- c(z1,z2)
+    x <- x[-1]
+  }else{
+    u <- runif(n/2)
+    v <- runif(n/2)
+    
+    z1 <- cos(2*pi*v)*sqrt(-2*log(u))
+    z2 <- cos(2*pi*u)*sqrt(-2*log(v))
+    x <- c(z1,z2)
+  }
   return(x)
 }
